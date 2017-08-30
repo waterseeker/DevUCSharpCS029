cfaf5cdc376ac1f04253f88b2a3397b42e98ce48
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-30 14:56:17 -0500

    first commit

---
+++ DevUCSharpCS-ASP_029/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DevUCSharpCS_ASP_029
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void fromGallonsRadio_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void fromQuartsRadio_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void fromPintsRadio_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void fromCupsRadio_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
